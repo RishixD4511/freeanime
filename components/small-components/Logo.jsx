6f5d820ed0e869fd7d11859998805dc8f4a854c4
RishixD4511 <122672252+RishixD4511@users.noreply.github.com>
2024-03-21 23:10:39 +0530

    Update Logo.jsx

--- components/small-components/Logo.jsx
+++ components/small-components/Logo.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Logo() {
   return (
     <Link href="/home">
-      <div className="text-3xl font-black hover:text-accent transition-all">Munchyroll</div>
+      <div className="text-3xl font-black hover:text-accent transition-all">Gradient Anime</div>
     </Link>
   );
 }
