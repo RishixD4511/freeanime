9e9a9b02c8d9282c755ff83231d88e857ea76e9f
RishixD4511 <122672252+RishixD4511@users.noreply.github.com>
2024-03-22 10:38:59 +0530

    Update Card.jsx

--- components/small-components/Card.jsx
+++ components/small-components/Card.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Link from "next/link";
+
 function Card({ data }) {
   return (
     <Link href={"/anime/" + data.animeId}>
-      <div className="     sm:p-3   ">
+      <div className="p-3 sm:p-3">
         <img
-          className=" hover:scale-105 transition-all w-full aspect-[5/7] object-cover rounded-lg"
+          className="hover:scale-105 transition-all w-full aspect-[5/7] object-cover rounded-lg border border-white"
           src={data.animeImg}
-          alt=""
+          alt={data.animeTitle}
         />
-
         <div>
-          <h4 className=" mt-3 font-bold  line-clamp-2">{data.animeTitle}</h4>
-
+          <h4 className="mt-3 font-bold line-clamp-2">{data.animeTitle}</h4>
           <p>Released: {data.releasedDate}</p>
         </div>
       </div>
